7e75583d66e51b039e5dd93c336f4d4acb311d9e
Spy68fer <khlybovds@gmail.com>
2015-04-20 21:11:02 +0300

    Добавлена первая практика

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-
-  echo('It works');
-  echo('Or ...?');
+    require_once("include/common.inc.php");
+  
+    $students = array
+    (
+      array("name" => "Alex", "age" => "20"),
+      array("name" => "Tanya", "age" => "21")
+    );
+  
+    $vars = array("students" => $students);
+    echo BuildPage("students.html", $vars);
